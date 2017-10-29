cfeb8ef575c06314a2d21361896287ddb59704e9
Lan Phan <lanc_phan@hotmail.com>
2017-10-29 15:57:52 -0400

    two each loop made to sum up all votes for each politician

--- ballot.rb
+++ ballot.rb
@@ -4,3 +4,20 @@ ballots = [{1 => 'Smudge', 2 => 'Tigger', 3 => 'Simba'},
            {1 =>'Boots', 2 => 'Felix', 3 => 'Bella'},
            {1 => 'Lucky', 2 => 'Felix', 3 => 'Bella'},
            {1 => 'Smudge', 2 => 'Simba', 3 => 'Felix'}]
+
+puts "\nThere are #{ballots.count} voting ballots."
+
+politician = Hash.new(0)
+ballots.each do |ballot|
+  ballot.each do |rank,name|
+    if rank == 1
+      politician[name] += 3
+    elsif rank == 2
+      politician[name] += 2
+    else
+      politician[name] += 1
+    end
+  end
+end
+
+p politician
